3f6381d6f7c49cafcd9025624b23969703b577c0
TechnoGeek01 <rsudharsangames@gmail.com>
2023-08-20 18:42:31 +0530

    added remove vertex method

--- graphs.js
+++ graphs.js
@@ -7,14 +7,26 @@ class Graph {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
 
-  addEdge(v1, v2) {
-    this.adjacencyList[v1].push(v2);
-    this.adjacencyList[v2].push(v1);
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
   }
 
-  removeEdge(v1, v2) {
-    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
-    this.adjacencyList[v2] = this.adjacencyList[v1].filter((v) => v !== v1);
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex1
+    );
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[v];
   }
 }
 
